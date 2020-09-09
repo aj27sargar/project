9ed7ec7da63e5a28e4e6d611a9af53316cd93194
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-09-09 03:10:11 +0300

    Style the product component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import './Home.css';
+import Product from './Product';
 
 function Home() {
   return (
-    <div>
+    <div className='home'>
       <div className='home__container'>
         <img className='home__image' src='https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_45M_v2_1x._CB432458380_.jpg' alt='amazon banner image' />
+      
+        <div className='home__row'>
+          <Product />
+          <Product />
+        </div>
+
+        <div className='home__row'>
+          <Product />
+          <Product />
+          <Product />
+        </div>
+
+        <div className='home__row'>
+          <Product />
+        </div>
       </div>
     </div>
   )
