b8ca328e7b608d35acd0439dd0d0a8c14d55a92e
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-11-16 23:03:13 +0300

    Add functions for logging in and creating an account

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 import { Link } from 'react-router-dom';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const signIn = e => {
+    e.preventDefault();
+  }
+
+  const register = e => {
+    e.preventDefault();
+  }
+
   return (
     <div className='login'>
       <Link to='/'>
@@ -16,10 +27,14 @@ function Login() {
         <h1>Sign-in</h1>
         <form>
           <h5>E-mail</h5>
-          <input type="text" />
+          <input type="text" value={email} onChange={e => setEmail(e.target.value)}/>
           <h5>Password</h5>
-          <input type="password" />
-          <button className='login__signInButton'>Sign In</button>
+          <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
+          <button 
+            className='login__signInButton'
+            type='submit'
+            onClick={signIn}
+          >Sign In</button>
         </form>
         <p>
           By signing-in you agree to Amazon Clone's 
@@ -27,7 +42,7 @@ function Login() {
           see our Privacy Notice, our Cookies Notice 
           and our Interest Based Ads.
         </p>
-        <button className='login__registerButton'>Create your Amazon account</button>
+        <button onClick={register} className='login__registerButton'>Create your Amazon account</button>
       </div>
     </div>
   )
