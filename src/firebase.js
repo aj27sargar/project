ac300ce14365b566c7935b027e85414e6e2e5be5
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-11-16 23:34:22 +0300

    Configure firebase to the app

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
   apiKey: "AIzaSyC-y0xwbxfvHsBRXm6CH6LTMMa52SGcbnQ",
   authDomain: "clone-27152.firebaseapp.com",
@@ -8,3 +10,11 @@ const firebaseConfig = {
   appId: "1:1066471477452:web:4d484689eed14c0acdf269",
   measurementId: "G-51XWMQQ2ER"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+const auth = firebase.auth();
+
+export { db, auth }
