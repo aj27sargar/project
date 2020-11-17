ca98e1fa784376feb230ee55dc1caf8692cea933
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-11-17 11:55:19 +0300

    Add styling to the payment component details

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Payment.css';
 import CheckoutProduct from './CheckoutProduct';
 import { useStateValue } from './StateProvider';
+import { Link } from 'react-router-dom';
 
 function Payment() {
 
@@ -10,6 +11,10 @@ function Payment() {
   return (
     <div className="payment">
       <div className="payment__container">
+        <h1>
+          Checkout (<Link to='/checkout'>{basket?.length} items</Link>)
+        </h1>
+
         <div className="payment__section">
           <div className="payment__title">
             <h3>Delivery Address</h3>
